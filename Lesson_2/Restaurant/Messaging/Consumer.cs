d967427214df733f8b0454cd67cc575dbdfae9a7
[VolAnton] <[_Tocuk@mail.ru]>
2022-09-27 00:36:30 +0300

    Домашнее задание №2

---
+++ Lesson_2/Restaurant/Messaging/Consumer.cs
@@ -0,0 +1,60 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging
+{
+    public sealed class Consumer : IDisposable
+    {
+        private readonly string _queueName; // название очереди
+        private readonly string _hostName; // хостнейм
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public Consumer(string queueName, string hostName)
+        {
+            _queueName = queueName;            
+            _hostName = hostName;
+
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = _hostName
+            };
+
+            _connection = factory.CreateConnection(); // создаем подключение
+            _channel = _connection.CreateModel();
+        }
+
+        public void Receive(EventHandler<BasicDeliverEventArgs> receiveCallback)
+        {
+            _channel.ExchangeDeclare(exchange: "direct_exchange",
+                type: "direct"); // объявляем обменник
+
+            _channel.QueueDeclare(queue: _queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null); // объявляем очередь
+
+            _channel.QueueBind(queue: _queueName,
+                exchange: "direct_exchange",
+                routingKey: _queueName); // биндим
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(_channel); // создаем consumer для канала
+            consumer.Received += receiveCallback; // добавляем обработчик события приема сообщения
+
+            _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer); // стартуем!
+        }
+
+        public void Dispose()
+        {
+            _connection?.Dispose();
+            _channel?.Dispose();
+        }
+
+    }
+}
---
+++ Lesson_2/Restaurant/Messaging/Producer.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging
+{
+    public sealed class Producer
+    {
+        private readonly string _queueName;
+        private readonly string _hostName;
+
+        public Producer(string queueName, string hostName)
+        {
+            _queueName = queueName;            
+            _hostName = hostName;
+        }
+
+        public void Send(string message)
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = _hostName
+            };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(
+                "direct_exchange",
+                "direct",
+                false,
+                false,
+                null
+            );
+
+            byte[] body = Encoding.UTF8.GetBytes(message); // формируем тело сообщения для отправки
+
+            channel.BasicPublish(exchange: "direct_exchange",
+                routingKey: _queueName,
+                basicProperties: null,
+                body: body); // отправляем сообщение
+        }
+
+    }
+}
